f33b9b4008fd6c5984bf686d734e6be1d9bc347c
Ben Dobbs <dobbsbp@gmail.com>
2017-05-20 17:28:51 +0700

    First

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-"# Arcade-Controller" 
+# Arcade Controller
+
+## What is this?
+
+This program is meant for arduino micro/leonardo type devices to allow
+keyboard input via usb connection.
+
+## Is it really that easy?
+
+If you're not afraid to connect a few wires together and make yourself some
+form of button to press, it is that easy.  
+
+## Why use a keypad grid instead of connecting buttons directly to GPIO and ground?
+
+Sometimes there just isn't enough GPIO pins available so you need to be crafty.  Imagine having only 5 GPIO pins available when you need to have 6 buttons.  In a grid that's 2 x 3, you can have your 6 buttons.  If you have 10 GPIO pins available, you can have a grid that is 5 x 5 which is more than double the amount of GPIO pins available.
+
+## So how do I wire this thing up?
+
+After you decide what GPIO pins you want to use, you wire things up according to your grid in the code.
+
+Here's an example of a 3 x 3 grid.
+
+--- | Pin 2 | Pin 3 | Pin 4
+Pin 6 | A | B | C
+Pin 7 | D | E | F
+Pin 8 | G | H | I
+
+Imagine the letters are your buttons.  Button A is connected to Pin 2 and Pin 6. Button E is connected to Pin 3 and Pin 7. Button I is connected to Pin 4 and Pin 8.
+
+## What kind of limitations are there?
+
+I have not tested this yet but I read that there is a limit of 10 keys being pressed at the same time.  For an arcade system, this probably not going to be a problem.  
+
+Lag.  There is currently some noticeable lag that needs to be addressed.  There's some minor adjustments to the code that will be made in the near future, but in the mean time things work ok and is playable.
+
+## Known problems?
+
+In Windows 10, the arduino micro pro might not be recognized as a HID device.  This means that the computer was not allowing the device to send in commands as if it were a device like a keyboard.
+
+My fix is kind of strange but it works.  Go to device manager, find your Arduino Micro under Ports. Right click, update driver, "Browse my computer for driver software", "Let me pick from a list of available drivers", and now you'll see a couple of options.  I selected the one that says "USB Serial Device" and voila! It works.  
+
+This is not an issue on RaspberryPi devices and most likely not a problem on linux systems either.
---
+++ src/arcade.ino
@@ -0,0 +1,95 @@
+/*  This file has been modified by Ben Dobbs. If you're interested in the
+original file, please take a look at examples from the Arduino IDE
+File --> Examples --> Keypad --> MultiKey
+
+What's different from the original Multikey program?
+
+The original MultiKey program would only send messages of what buttons were
+being pressed via serial monitor, not actually send commands telling your
+computer via USB that a button has been pressed.
+
+Limitations?
+
+10 keys can be pressed at the same time.
+Some lag.
+
+I'll be working on addressing the lag issue in future updates.  For now, this
+is a working sketch.
+*/
+
+#include <Keypad.h>
+#include <Keyboard.h>
+
+const byte ROWS = 4; //four rows
+const byte COLS = 3; //three columns
+char keys[ROWS][COLS] = {
+{'1','2','3'},
+{'4','5','6'},
+{'7','8','9'},
+{'*','0','#'}
+};            // These keys can be replaced with whatever key you'd like.
+              // For ones you can't type like up arrow, please visit
+              // https://www.arduino.cc/en/Reference/KeyboardModifiers
+byte rowPins[ROWS] = {6, 7, 8, 9}; //connect to the row pinouts of the kpd
+byte colPins[COLS] = {14, 16, 10}; //connect to the column pinouts of the kpd
+
+// You can create a larger grid, just be sure to update the ROWS and COLS values
+// and also what pinouts you're using above.
+
+Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+unsigned long loopCount;
+unsigned long startTime;
+String msg;
+
+
+void setup() {
+    Serial.begin(9600);  // Useful for debuging, otherwise not neccisary.
+    loopCount = 0;
+    startTime = millis();
+    msg = "";
+    Keyboard.begin(); // Begin up Keyboard emulation
+    }
+
+
+void loop() {
+
+    loopCount++;
+    if ( (millis()-startTime)>5000 ) {
+        Serial.print("Average loops per second = ");
+        Serial.println(loopCount/5);
+        startTime = millis();
+        loopCount = 0;
+    }
+
+    // Fills kpd.key[ ] array with up-to 10 active keys.
+    // Returns true if there are ANY active keys.
+    if (kpd.getKeys())
+    {
+        for (int i=0; i<LIST_MAX; i++)   // Scan the whole key list.
+        {
+            if ( kpd.key[i].stateChanged )   // Only find keys that have changed state.
+            {
+                switch (kpd.key[i].kstate) {  // Report active key state : IDLE, PRESSED, HOLD, or RELEASED
+                    case PRESSED:
+                    msg = " PRESSED.";
+                    Keyboard.press(kpd.key[i].kchar); // Send keyboard press
+                break;
+                    case HOLD:
+                    msg = " HOLD.";
+                break;
+                    case RELEASED:
+                    msg = " RELEASED.";
+                    Keyboard.release(kpd.key[i].kchar); // Send keyboard release
+                break;
+                    case IDLE:
+                    msg = " IDLE.";
+                }
+                Serial.print("Key ");
+                Serial.print(kpd.key[i].kchar);
+                Serial.println(msg);
+
+            }
+        }
+    }
+}  // End loop
